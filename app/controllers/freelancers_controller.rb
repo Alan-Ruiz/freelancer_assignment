a30feb315c2d954e1db98354cbe1ec60f4ca05cb
Alan-Ruiz <gheryk90@gmail.com>
2022-07-27 20:24:38 +0100

    css boxes for users

--- app/controllers/freelancers_controller.rb
+++ app/controllers/freelancers_controller.rb
@@ -2,12 +2,13 @@ class FreelancersController < ApplicationController
   before_action :set_freelancer, only: [:show, :update, :edit, :destroy]
   
   def index
-    if params[:query1].present?
+    if params[:query2].present?
       query = Freelancer.where(rate: params[:query1]..params[:query2])
       @pagy, @freelancers = pagy(query.order(feature: :desc))
     else
       # @pagy, @freelancers = pagy(Freelancer.includes(:user).where(id: all_freelancers_ids))
-      @pagy, @freelancers = pagy(all_freelancers_ids)
+      # @pagy, @freelancers = pagy(all_freelancers_ids)
+      @pagy, @freelancers = pagy(Freelancer.order(feature: :desc))
       #preload users because that is how it is shown on the index page (freelance.user)
     end
     # @pagy, @records = pagy(Product.some_scope)
@@ -57,14 +58,14 @@ class FreelancersController < ApplicationController
     params.require(:freelancer).permit(:feature, :bio, :rate, :user_id)
   end
 
-  def all_freelancers_ids
-    Rails.cache.fetch("all_freelancers", expires_in: 24.hours) do
-      puts "HEELO IM HERE YOOOOOOOOO"
-      Freelancer.includes(:user).order(feature: :desc)
-    end
+  # def all_freelancers_ids
+  #   Rails.cache.fetch("all_freelancers", expires_in: 24.hours) do
+  #     puts "HEELO IM HERE YOOOOOOOOO"
+  #     Freelancer.includes(:user).order(feature: :desc)
+  #   end
 
     #caching activerecords
-  end
+  # end
 
   # def all_freelancers
   #   Rails.cache.fetch("all_freelancers", expires_in: 24.hours) do
